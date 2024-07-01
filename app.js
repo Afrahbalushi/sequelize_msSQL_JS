08cad985002a367f9544486f760f2174b6c3ca6f
Afrah Ali <116621717+Afrahbalushi@users.noreply.github.com>
2024-07-01 16:29:50 +0400

    delete a record from table method

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const db = require('./models');
 
 app.use(express.json());
 
-
 db.sequelize.sync()
   .then(() => {
     console.log('Database synced successfully.');
@@ -12,7 +11,6 @@ db.sequelize.sync()
   .catch(err => {
     console.error('Error syncing database:', err);
   });
-
 
 app.get('/', (req, res) => {
   res.send('Hello, Server is working fine!');
@@ -31,6 +29,21 @@ app.get('/users', async (req, res) => {
   try {
     const users = await db.User.findAll();
     res.json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await db.User.findByPk(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    await user.destroy();
+    res.send('Row has been deleted!');
+    res.status(204).end();
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
